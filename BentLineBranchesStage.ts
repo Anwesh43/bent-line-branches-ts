d3b791a9ffdf480645af2f276f0bcce2a2984d17
Anwesh43 <anweshthecool0@gmail.com>
2019-05-19 12:50:32 +0530

    adding BentLineBranches class

--- BentLineBranchesStage.ts
+++ BentLineBranchesStage.ts
@@ -197,3 +197,26 @@ class BLBNode {
         return this
     }
 }
+
+class BentLineBranches {
+    root : BLBNode = new BLBNode(0)
+    curr : BLBNode = this.root
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.root.draw(context)
+    }
+
+    update(cb : Function) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : Function) {
+        this.curr.startUpdating(cb)
+    }
+}
