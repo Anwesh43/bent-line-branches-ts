bd4642eed832957052323e2b20ad2ba0ec126a8b
Anwesh43 <anweshthecool0@gmail.com>
2019-05-19 12:16:21 +0530

    adding ScaleUtil class

--- BentLineBranchesStage.ts
+++ BentLineBranchesStage.ts
@@ -9,3 +9,27 @@ const backColor : string = "#BDBDBD"
 const nodes : number = 5
 const parts : number = 2
 const lines : number = 2
+
+class ScaleUtil {
+
+    static maxScale(scale : number, i : number, n : number) : number {
+        return  Math.max(0, scale - i / n)
+    }
+
+    static divideScale(scale : number, i : number, n : number) : number {
+        return Math.min(1 / n, ScaleUtil.maxScale(scale, i , n)) * n
+    }
+
+    static scaleFactor(scale : number) : number {
+        return Math.floor(scale / scDiv)
+    }
+
+    static mirrorValue(scale : number, a : number, b : number) : number {
+        const k : number = ScaleUtil.scaleFactor(scale)
+        return (1 - k) / a + k / b
+    }
+
+    static updateValue(scale : number, dir : number, a : number, b : number) : number {
+        return ScaleUtil.mirrorValue(scale, a, b) * dir * scGap
+    }
+}
